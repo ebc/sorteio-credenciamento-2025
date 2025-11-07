f231197dd2fdf8cf48376f1af8fb21393b231613
vinicius.monteiro <vinicius.monteiro@ebc.com.br>
2025-11-07 18:48:45 -0300

    Incluido as participantes abaixo: Inscrição - 229 - CNPJ - 31399764000125 - Razão Social - Log Produções & Filmes Ltda. Inscrição - 204 - CNPJ - 23883894000100 - Razão Social - Caninópolis Produções Audiovisuais Ltda. Retirado as participantes abaixo: Inscricao - 27  - CNPJ - 2759786000150  - RazaoSocial - Flora Filmes e Vídeos Produções Artísticas LTDA Inscricao - 148 - CNPJ - 35485214000180 - RazaoSocial - Dogana Boutique Digital & Serviços de Produção Cinematográficas Ltda Inscricao - 149 - CNPJ - 2877208600019  - RazaoSocial - Plongee 360 Produção e Arte Ltda Inscricao - 220 - CNPJ - 97544735000118 - RazaoSocial - Maré Alta Filmes Ltda

--- src/participantes.js
+++ src/participantes.js
@@ -1,8 +1,18 @@
 const pareceristas = [
-  { 'Inscricao': 27, 'CNPJ': '2759786000150', 'RazaoSocial': 'Flora Filmes e Vídeos Produções Artísticas LTDA' },
-  { 'Inscricao': 82, 'CNPJ': '23705524000183', 'RazaoSocial': 'Toca Filmes Comunicação Ltda' },
-  { 'Inscricao': 148, 'CNPJ': '35485214000180', 'RazaoSocial': 'Dogana Boutique Digital & Serviços de Produção Cinematográficas Ltda' },
-  { 'Inscricao': 149, 'CNPJ': '2877208600019', 'RazaoSocial': 'Plongee 360 Produção e Arte Ltda' },
-  { 'Inscricao': 220, 'CNPJ': '97544735000118', 'RazaoSocial': 'Maré Alta Filmes Ltda' },
+  {
+    Inscricao: 82,
+    CNPJ: '23705524000183',
+    RazaoSocial: 'Toca Filmes Comunicação Ltda.'
+  },
+  {
+    Inscricao: 204,
+    CNPJ: '23883894000100',
+    RazaoSocial: 'Caninópolis Produções Audiovisuais Ltda.'
+  },
+  {
+    Inscricao: 229,
+    CNPJ: '31399764000125',
+    RazaoSocial: 'Log Produções & Filmes Ltda.'
+  }
 ];
 export default pareceristas;
